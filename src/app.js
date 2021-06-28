9485a4bcd87e57400513d2a6bb8bce4b20e482b9
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-28 16:27:32 -0400

    Minor updates

--- src/app.js
+++ src/app.js
@@ -3,19 +3,21 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const errorHandler = require("./errors/errorHandler");
-const notFound = require("./errors/notFound");
 
 
 //Handlers
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
 
 
 
 //Routers
 
 
-
+//Use JSON format for data return
 app.use(express.json());
+
+//Use CORS to allow all incoming requests
 app.use(cors());
 
 //Direct request based on URL
