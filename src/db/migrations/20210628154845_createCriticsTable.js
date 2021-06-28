0db2c04dde1c9005e93fd501f831fa539b320e17
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-28 16:01:33 -0400

    Removed timestamp from critics table, it didn't make sense

--- src/db/migrations/20210628154845_createCriticsTable.js
+++ src/db/migrations/20210628154845_createCriticsTable.js
@@ -4,7 +4,6 @@ exports.up = function (knex) {
     table.string("preferred_name");
     table.string("surname");
     table.string("organization_name");
-    table.timestamps(true, true);
   })
 };
 
