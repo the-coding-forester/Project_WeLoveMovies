7ed978363a5099eb4813b9d1667eab4a0f856db8
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-29 16:24:33 -0400

    List functionality support

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,11 +1,25 @@
 const knex = require("../db/connection");
 
-const tableName = "movies";
+
+//List all movies in the database
+const list = () => {
+  return knex('movies').select('*');
+}
+
+//List all movies where is showing is true
+//Will need to connect movies table and movies_theaters table
+const listMoviesIsShowingTrue = () => {
+  return knex('movies as m')
+    .join('movies_theaters as mt', 'm.movie_id', 'mt.movie_id')
+    .select('m.*')
+    .where({ 'mt.is_showing': true })
+    .groupBy('m.title')
+}
 
 //read function
 
-//list function
 
 module.exports = {
-
+  list,
+  listMoviesIsShowingTrue,
 };
