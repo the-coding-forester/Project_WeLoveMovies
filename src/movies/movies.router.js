309e732c82d035c791f4f1a7da150910d0d919f6
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-29 17:04:16 -0400

    Added router for to return list of theaters where a movie is playing

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -4,13 +4,26 @@ const methodNotAllowed = require('../errors/methodNotAllowed')
 
 const router = Router();
 
+
+//Route for ('/movies/:movieId/theaters')
+//Only the GET method is allowed
+router
+  .route("/:movies/theaters")
+  .get(controller.getTheatersShowingMovie)
+  .all(methodNotAllowed);
+
+//Route for ('/movies/:movieId')
+//Only the GET method is allowed
+router
+  .route("/:movieId")
+  .get(controller.read)
+  .all(methodNotAllowed)
+
 //Route for ('/movies')
 //Only the GET method is allowed
 router
   .route("/")
   .get(controller.list)
   .all(methodNotAllowed)
-
-
 
 module.exports = router;
