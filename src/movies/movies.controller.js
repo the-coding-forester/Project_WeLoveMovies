ab8d6bbe151522a00eca8885868701aff270c27b
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-29 16:19:58 -0400

    list functionality for movies complete

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -3,7 +3,27 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 //MIDDLEWARE
 
+//Route for ('/')
+const list = async (req, res) => {
+  const { is_showing } = req.query;
+
+  if (is_showing === 'true') {
+    //if is showing is true then return the
+    const data = await service.listMoviesIsShowingTrue();
+    return res.json({ data });
+  }
+  //return all movies by default
+  const data = await service.list();
+  res.json({ data: data });
+}
+
+//Route for ('/:movieId')
+const read = async (req, res, next) => {
+  const data = res.local.movie;
+  res.json({ data: data[0] });
+}
 
 module.exports = {
-
+  list: asyncErrorBoundary(list),
+  read
 };
