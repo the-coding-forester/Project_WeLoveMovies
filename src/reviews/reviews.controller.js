5e954e7019fd97a082ff2870553d40280c829a3d
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-30 15:18:37 -0400

    basic setup

---
+++ src/reviews/reviews.controller.js
@@ -0,0 +1,8 @@
+const service = require('./reviews.service');
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+
+//MIDDLEWARE
+
+module.exports = {
+
+};
---
+++ src/reviews/reviews.router.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const controller = require('./reviews.controller');
+const methodNotAllowed = require('../errors/methodNotAllowed');
+
+const router = Router();
+
+router
+  .route('/')
+
+modules.exports = router;
