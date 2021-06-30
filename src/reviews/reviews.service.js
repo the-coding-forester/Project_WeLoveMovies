acc6219429376ca44dbdf54948cd3f0b09b01a38
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-30 16:11:03 -0400

    destroy/delete query created

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,6 +1,17 @@
 const knex = require("../db/connection");
 
+//Delete a single review by ID
+const destroy = (reviewId) => {
+  return knex('reviews')
+    .where({ review_id: reviewId })
+    .del();
+}
+
+
+
+
+
 
 module.exports = {
-
+  destroy,
 };
