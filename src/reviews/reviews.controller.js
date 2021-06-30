d927d2b51a7c17ae9e21e6c586a95ddbc4c82b26
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-30 16:11:57 -0400

    destroy middleware created, functional, and sending status 204

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -3,6 +3,19 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
 
 //MIDDLEWARE
 
-module.exports = {
+//Route for ('/reviews/:reviewId)
+const destroy = async (req, res, next) => {
+  const { reviewId } = req.params;
+  await service.destroy(reviewId);
+  res.sendStatus(204)
+}
 
+
+
+
+
+module.exports = {
+  destroy: [
+    asyncErrorBoundary(destroy)
+  ]
 };
