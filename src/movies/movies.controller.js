83886a437619ea131f7fb43b9d92ba536ff8494d
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-30 15:05:05 -0400

    Updated route for get movie review with critic information and restructured the returning data

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -44,10 +44,32 @@ const getTheatersShowingMovie = async (req, res, next) => {
   res.json({ data: data });
 }
 
+//Route for ('/movies/:movieId/theaters')
+//Restructure the data so that the critic data is in a nested object
 const getMovieReviewsWithCritics = async (req, res, next) => {
   const { movieId } = req.params;
   const data = await service.getMovieReviewsWithCritics(movieId);
-  res.json({ data: data });
+  const updatedData = data.map((review) => {
+    return {
+      "review_id": review.review_id,
+      "content": review.content,
+      "score": review.score,
+      "created_at": review.created_at,
+      "updated_at": review.updated_at,
+      "critic_id": review.critic_id,
+      "movie_id": review.movie_id,
+      "critic": {
+        "critic_id": review.critic_id,
+        "preferred_name": review.preferred_name,
+        "surname": review.surname,
+        "organization_name": review.organization_name,
+        "created_at": review.critic_created,
+        "updated_at": review.critic_updated
+      }
+    }
+  })
+
+  res.json({ data: updatedData });
 }
 
 module.exports = {
