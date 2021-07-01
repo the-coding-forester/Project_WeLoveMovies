745c66298820a89ed15cc9663be6549788b72f9f
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-30 23:41:42 -0400

    All routes for reviews are set up and working

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -8,9 +8,10 @@ const router = Router();
 //Methods GET, UPDATE, and DELETE supported
 router
   .route("/:reviewId")
+  .get(controller.read)
   .delete(controller.destroy)
+  .put(controller.update)
   .all(methodNotAllowed)
-
 
 
 module.exports = router;
