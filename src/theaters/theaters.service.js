160ae1aedc2475093b3da831fdb30eb066c56117
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-01 01:13:11 -0400

    Created 2 queries, one to return all theaters and one to return movies showing in a give theater

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,10 +1,20 @@
 const knex = require("../db/connection");
 
 const list = () => {
-  return knex('theaters').select('*');
+  return knex('theaters as t')
+    .select('*')
+}
+
+const listMoviesShowingAtTheater = (theaterId) => {
+  return knex('movies as m')
+    .join('movies_theaters as mt', 'm.movie_id', 'mt.movie_id')
+    .select('m.*')
+    .where({ 'mt.is_showing': true, 'mt.theater_id': theaterId })
+    .groupBy('m.title')
 }
 
 
 module.exports = {
   list,
+  listMoviesShowingAtTheater
 }
