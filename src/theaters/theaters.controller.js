096f72a1b6ddde738991f5bfffcec0d51d1bc70e
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-01 01:15:09 -0400

    Created middleware that uses both service queries to return all theaters with an array of movies showing in each one

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -2,12 +2,20 @@ const service = require('./theaters.service');
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
 
 //MIDDLEWARE
+//Route for ('/theaters)
 
-
-
-
-
+const list = async (req, res) => {
+  const data = await service.list();
+  const updatedData = data.map(async (theater) => {
+    return {
+      ...theater,
+      'movies': await service.listMoviesShowingAtTheater(theater.theater_id)
+    }
+  })
+  const finalData = Promise.all(updatedData)
+  res.json({ data: await finalData });
+}
 
 module.exports = {
-
+  list: asyncErrorBoundary(list)
 }
