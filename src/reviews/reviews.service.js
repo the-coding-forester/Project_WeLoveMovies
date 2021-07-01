3ccc22a971f09120efbb1a97d63d3dee73c3c189
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-30 23:44:55 -0400

    Function created to run 2 queries, 1st to update review, then to join tables so review and critic data can be returned

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -7,11 +7,29 @@ const destroy = (reviewId) => {
     .del();
 }
 
+//Return a single review by ID
+//Needed for the reviewExists middleware
+const read = (reviewId) => {
+  return knex('reviews')
+    .select('*')
+    .where({ review_id: reviewId })
+    .first();
+}
 
-
-
+//Update a single review by ID
+const update = async (updatedReview) => {
+  await knex('reviews as r')
+    .where({ "r.review_id": updatedReview.review_id })
+    .update(updatedReview)
+  return knex("reviews as r")
+    .join('critics as c', 'c.critic_id', 'r.critic_id')
+    .select('*', 'c.created_at as critic_created', 'c.updated_at as critic_updated')
+    .where({ "r.review_id": updatedReview.review_id })
+}
 
 
 module.exports = {
+  read,
   destroy,
+  update,
 };
